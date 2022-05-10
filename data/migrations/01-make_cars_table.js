a3c2db1bad1ba0ab06f92a4e6ebd510426f62f70
unknown <ryancare@protonmail.com>
2022-05-09 19:54:41 -0500

    Added migration schema

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,17 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id');
+    tbl.text('vin', 17).unique().notNullable();
+    tbl.text('make', 64).notNullable();
+    tbl.text('model', 64).notNullable();
+    tbl.decimal('mileage').notNullable();
+    tbl.text('title', 128);
+    tbl.text('transmission', 128);
+  })
 };
 
 exports.down = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.dropTableIfExists('cars');
 };
